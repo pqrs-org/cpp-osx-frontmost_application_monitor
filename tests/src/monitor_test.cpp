ffb2ea29463eb9e22b66c94b4599e52fe13d8d45
Takayama Fumihiko <tekezo@pqrs.org>
2019-01-11 20:59:50 +0900

    improved objc part to use own NSOperationQueue

--- tests/src/monitor_test.cpp
+++ tests/src/monitor_test.cpp
@@ -1,4 +1,5 @@
 #include <catch2/catch.hpp>
+#include <iostream>
 
 #include <pqrs/osx/frontmost_application_monitor.hpp>
 
@@ -6,9 +7,21 @@ TEST_CASE("monitor") {
   auto time_source = std::make_shared<pqrs::dispatcher::hardware_time_source>();
   auto dispatcher = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
 
-  auto monitor = std::make_shared<pqrs::osx::frontmost_application_monitor::monitor>(dispatcher);
+  for (int i = 0; i < 10000; ++i) {
+    auto wait = pqrs::make_thread_wait();
 
-  monitor = nullptr;
+    auto monitor = std::make_shared<pqrs::osx::frontmost_application_monitor::monitor>(dispatcher);
+
+    monitor->frontmost_application_changed.connect([wait](auto&& application_ptr) {
+      wait->notify();
+    });
+
+    monitor->async_start();
+
+    wait->wait_notice();
+
+    monitor = nullptr;
+  }
 
   dispatcher->terminate();
   dispatcher = nullptr;
