86626afa2eca65715d33c92e91617e5b72e6a779
Takayama Fumihiko <tekezo@pqrs.org>
2023-08-11 21:12:21 +0900

    Add comment

--- src/pqrs/osx/frontmost_application_monitor/impl.swift
+++ src/pqrs/osx/frontmost_application_monitor/impl.swift
@@ -1,3 +1,7 @@
+// (C) Copyright Takayama Fumihiko 2023.
+// Distributed under the Boost Software License, Version 1.0.
+// (See https://www.boost.org/LICENSE_1_0.txt)
+
 import AppKit
 
 private actor Monitor {
