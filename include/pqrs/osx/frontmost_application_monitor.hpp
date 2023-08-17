d259578710015ff01d4e47e44aad7db6d3085dd1
Takayama Fumihiko <tekezo@pqrs.org>
2023-08-18 08:44:03 +0900

    Version 4.1.0

--- include/pqrs/osx/frontmost_application_monitor.hpp
+++ include/pqrs/osx/frontmost_application_monitor.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::frontmost_application_monitor v4.0
+// pqrs::osx::frontmost_application_monitor v4.1
 
 // (C) Copyright Takayama Fumihiko 2019.
 // Distributed under the Boost Software License, Version 1.0.
